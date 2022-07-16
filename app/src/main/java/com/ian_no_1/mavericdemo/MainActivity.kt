bfd401ff9b98e38f65e47a940ffafc2b427b376a
Ian Pan <g-hamu@hotmail.com>
2022-07-16 18:45:08 +0800

    feat: enable view binding

--- app/src/main/java/com/ian_no_1/mavericdemo/MainActivity.kt
+++ app/src/main/java/com/ian_no_1/mavericdemo/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.ian_no_1.mavericdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ian_no_1.mavericdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
+
+
+
+
 }
